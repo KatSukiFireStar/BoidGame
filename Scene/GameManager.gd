4213f53ea4c511e3a4625a0d3f44c266a6e66598
KaTSukiFireStar <78344193+KatSukiFireStar@users.noreply.github.com>
2024-11-07 17:39:51 +0100

    Create boid script

---
+++ Scene/GameManager.gd
@@ -0,0 +1,65 @@
+extends Node2D
+
+@export
+var nbBoids : int
+
+@export
+var boidPrefab : PackedScene
+
+var maxX : float
+var maxY : float
+var boids : Array
+var rng : RandomNumberGenerator
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+
+	var screenSize = get_tree().root.content_scale_size
+	rng = RandomNumberGenerator.new()
+	
+	maxX = screenSize.x
+	maxY = screenSize.y
+	
+	for i in range(0, nbBoids):
+		var boid : Node2D = boidPrefab.instantiate()
+		
+		var posX : float = rng.randf_range(0, maxX)
+		var posY : float = rng.randf_range(0, maxY) 
+		
+		boid.transform.origin = Vector2(posX, posY)
+		
+		boid.maxX = maxX
+		boid.maxY = maxY
+		
+		boids.append(boid)
+		add_child(boid)
+		
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	for boid in boids:
+		var closeBoids : Array
+		for otherBoid in boids:
+			if boid == otherBoid:
+				continue
+			var distance = boid.distance(otherBoid)
+			if distance < 800:
+				closeBoids.append(otherBoid)
+		
+		boid.moveCloser(closeBoids)
+		boid.moveWith(closeBoids)
+		boid.moveAway(closeBoids, 20)
+		
+		var border = 25
+		if boid.transform.origin.x < border and boid.velocity.x < 0:
+			boid.velocity.x = - boid.velocity.x * rng.randf()
+		if boid.transform.origin.x > maxX - border and boid.velocity.x > 0:
+			boid.velocity.x = - boid.velocity.x * rng.randf()
+		if boid.transform.origin.y < border and boid.velocity.y < 0:
+			boid.velocity.y = - boid.velocity.y * rng.randf()
+		if boid.transform.origin.y > maxY - border and boid.velocity.y > 0:
+			boid.velocity.y = - boid.velocity.y * rng.randf()
+			
+		boid.move()
+		
---
+++ Script/boid.gd
@@ -0,0 +1,93 @@
+extends Node2D
+
+var maxX : int
+var maxY : int
+
+var velocity : Vector2
+
+func move() -> void:
+	if abs(velocity.x) > 10 or abs(velocity.y) > 10:
+		var scaleFactor = 10 / max(abs(velocity.x), abs(velocity.y))
+		
+		velocity.x *= scaleFactor
+		velocity.y *= scaleFactor
+	
+	if transform.origin.x > 0 and transform.origin.x < maxX:
+		transform.origin.x += velocity.x
+	if transform.origin.y > 0 and transform.origin.y < maxY:
+		transform.origin.y += velocity.y
+
+func distance(boid: Node2D) -> float:
+	var distX = transform.origin.x - boid.transform.origin.x
+	var distY = transform.origin.y - boid.transform.origin.y
+	return sqrt(distX * distX + distY * distY)
+
+
+func moveCloser(boids: Array) -> void:
+	if len(boids) < 1: return
+	
+	var avgX = 0
+	var avgY = 0
+	
+	for boid in boids:
+		if boid.transform.origin.x == transform.origin.x and boid.transform.origin.y == transform.origin.y:
+			continue
+			
+		avgX += (transform.origin.x - boid.transform.origin.x)
+		avgY += (transform.origin.y - boid.transform.origin.y)
+		
+	avgX /= len(boids)
+	avgY /= len(boids)
+	
+	velocity = Vector2(velocity.x - (avgX / 10), velocity.y - (avgY / 10))
+	
+func moveWith(boids: Array) -> void:
+	if len(boids) < 1: return
+	
+	var avgX = 0
+	var avgY = 0
+	
+	for boid in boids:
+		if boid.transform.origin.x == transform.origin.x and boid.transform.origin.y == transform.origin.y:
+			continue
+			
+		avgX += (transform.origin.x - boid.transform.origin.x)
+		avgY += (transform.origin.y - boid.transform.origin.y)
+		
+	avgX /= len(boids)
+	avgY /= len(boids)
+	
+	velocity = Vector2(velocity.x + (avgX / 40), velocity.y + (avgY / 40))
+
+
+func moveAway(boids: Array, minDist : float) -> void:
+	if len(boids) < 1: return
+	
+	var distX = 0
+	var distY = 0
+	var numClose = 0
+	
+	for boid in boids:
+		var dist = distance(boid)
+		if dist < minDist:
+			numClose += 1
+			var xDiff = (transform.origin.x - boid.transform.origin.x)
+			var yDiff = (transform.origin.y - boid.transform.origin.y)
+			
+			if xDiff >= 0:
+				xDiff = sqrt(minDist) - xDiff
+			elif xDiff < 0:
+				xDiff = -sqrt(minDist) - xDiff
+				
+			if yDiff >= 0:
+				yDiff = sqrt(minDist) - yDiff
+			elif yDiff < 0:
+				yDiff = -sqrt(minDist) - yDiff
+				
+			distX += xDiff
+			distY += yDiff
+			
+			if numClose == 0:
+				return
+				
+			velocity = Vector2(velocity.x - distX / 5, velocity.y - distY / 5)
